c4531d0a9eef6cbeee716b23846b0fbfa74e192d
ahmdnz <65539220+ahmdnz@users.noreply.github.com>
2020-12-13 17:48:53 +0330

    Update and rename main_code.py to play.py

--- play.py
+++ play.py
@@ -1,10 +1,13 @@
 import pyautogui
 import pyscreenshot as ImageGrab
 
+# select the area of screen for making the screenshot, this area must cover the 6 branches
 bbox = (1059, 0, 1060, 500)
+# select the Y coordinates of branches
 points = [(0, 455), (0, 387), (0, 319), (0, 255), (0, 186), (0, 119)]
 
 
+# the coordinates of 'Play', 'left' and 'right' buttons must be defined
 def play():
     pyautogui.click(1031, 645)
 
@@ -17,16 +20,16 @@ def click_left():
     pyautogui.click(960, 634)
 
 
-def get_steps():
+def get_colors():
     im = ImageGrab.grab(bbox=bbox).convert('RGB')
-    return [im.getpixel(s) for s in points]
+    return [im.getpixel(point) for point in points]
 
 
 play()
-count = 0
+count = 0  # you can define the count to control the score!
 while count < 200:
-    for pixels in get_steps():
-        if sum(pixels) == 333:
+    for color in get_colors():
+        if sum(color) == 333:
             click_left()
             click_left()
             count += 2
